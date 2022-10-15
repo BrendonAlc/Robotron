39a86cff324c67da94cda07124a41a4a2f772b7e
BrendonAlc <brendon_rodrigo@hotmail.com>
2022-10-15 16:50:02 -0300

    Adicionando função para os botões de adição e subtração, adicionado função para utilizar a constante peca e atualizando as estatisticas conforme for adicionando atributos ao robotron

--- js/main.js
+++ js/main.js
@@ -1,6 +1,67 @@
-const robotron = document.querySelector("#robotron")
-robotron.addEventListener("click", dizOi)
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
 
-function dizOi(){
-    console.log("oi")
+//Objeto com estatisticas do robotron
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
 }
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode) //parentNode busca o elemento PAI 
+        atualizaEstatistica(evento.target.dataset.peca)
+    })
+})
+
+
+
+function manipulaDados (operacao, controle){
+    const peca = controle.querySelector("[data-contador]")
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1
+    }
+    else
+    {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+//Atualiza estatistica
+function atualizaEstatistica(peca) {
+    estatisticas.forEach( (elemento) => { //buscando elemento no objeto estatistica
+        //buscando elemento que esta em formato string e convertendo para inteiro e atualizando (somando) a base de estatistica a cada peca adicionada
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatisticas]
+    })
+}
+
+
